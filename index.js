66a1bf5a92244f1b80f67fe02ba5d3587095f980
Davis Goff <davisparkergoff@gmail.com>
2021-04-01 20:24:39 -0400

    Add toggling between the mode types

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const toggle = document.querySelector("#mode-toggle");
-toggle.addEventListener("click", modeToggle);
-
 let isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-if (!isDark) {
-  let root = document.body;
-  root.classList.toggle("lightMode");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("mode-toggle");
+  toggle.addEventListener("click", modeToggle);
+
+  if (!isDark) {
+    let root = document.body;
+    root.classList.toggle("lightMode");
+  }
+});
 
 function modeToggle() {
   isDark = !isDark;
