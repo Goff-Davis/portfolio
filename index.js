83686a91eef4e707c150d85b7cfe9f315796be50
Davis Goff <davisparkergoff@gmail.com>
2021-04-02 21:33:48 -0400

    Style linting and prefixing

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("mode-toggle");
+  const toggle = document.getElementById("js-mode-toggle");
   toggle.addEventListener("click", modeToggle);
 
-  const burger = document.getElementById("burger");
+  const burger = document.getElementById("js-burger");
   burger.addEventListener("click", burgerToggle);
 
   if (!isDark) {
@@ -21,10 +21,10 @@ function modeToggle() {
 }
 
 function burgerToggle() {
-  const bar1 = document.getElementById("bar1");
-  const bar2 = document.getElementById("bar2");
-  const bar3 = document.getElementById("bar3");
-  const navList = document.getElementById("nav-list");
+  const bar1 = document.getElementById("js-bar1");
+  const bar2 = document.getElementById("js-bar2");
+  const bar3 = document.getElementById("js-bar3");
+  const navList = document.getElementById("js-nav-list");
 
   bar1.classList.toggle("bar1Open");
   bar2.classList.toggle("bar2Open");
