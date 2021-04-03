76011588e860fec71b0b05a5164dbb5a7f1a6842
Davis Goff <davisparkergoff@gmail.com>
2021-04-02 21:04:59 -0400

    Create hamburger menu

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.getElementById("mode-toggle");
   toggle.addEventListener("click", modeToggle);
 
+  const burger = document.getElementById("burger");
+  burger.addEventListener("click", burgerToggle);
+
   if (!isDark) {
     let root = document.body;
     root.classList.toggle("lightMode");
@@ -15,4 +18,16 @@ function modeToggle() {
 
   let root = document.body;
   root.classList.toggle("lightMode");
+}
+
+function burgerToggle() {
+  const bar1 = document.getElementById("bar1");
+  const bar2 = document.getElementById("bar2");
+  const bar3 = document.getElementById("bar3");
+  const navList = document.getElementById("nav-list");
+
+  bar1.classList.toggle("bar1Open");
+  bar2.classList.toggle("bar2Open");
+  bar3.classList.toggle("bar3Open");
+  navList.classList.toggle("navListOpen");
 }
